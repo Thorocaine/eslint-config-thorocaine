c6db2e4f5af0e9c70abae8e972b4c3102ffab98c
Jonathan Peel <me@jonathanpeel.co.za>
2019-10-20 11:34:56 +0800

    update rules

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ module.exports = {
         'plugin:jsx-a11y/recommended',
         'plugin:react/recommended',
     ],
+    globals: {module: false},
     parser: '@typescript-eslint/parser',
     plugins: [ '@typescript-eslint', 'prettier', 'jsx-a11y', 'react' ],
     settings: {
@@ -15,9 +16,30 @@ module.exports = {
         'import/resolver': { typescript: {} },
     },
     rules: {
+        "@typescript-eslint/explicit-function-return-type": [0],
+        "@typescript-eslint/indent": [2, 2],
+        "@typescript-eslint/no-use-before-define": [2, { "functions": false }],
+
+        "react/forbid-prop-types": 2,
+        "react/prop-types": 0,
+
         'brace-style': [2, 'allman', { "allowSingleLine": true }],
-        'quotes': [2, 'single'],
-        'react/forbid-prop-types': 2,
-        'react/prop-types': 0,
+        "quotes": [2, "single"],
+        
+        "jsx-a11y/label-has-for": [
+            2,
+            {
+                "components": [
+                    "Label"
+                ],
+                "required": {
+                    "some": [
+                        "nesting",
+                        "id"
+                    ]
+                },
+                "allowChildren": false
+            }
+        ],
     },
 };
